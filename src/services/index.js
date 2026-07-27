cfa5c3e783868a1a838a22e3e3daed0618e33ce1
apoorvm884-netizen <apoorvm884@gmail.com>
2026-07-27 17:16:48 +0530

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -12,6 +12,7 @@ export { CourseService } from './course-service.js';
 export { EnrollmentService } from './enrollment-service.js';
 export { NotificationService } from './notification-service.js';
 export { AccountService } from './account-service.js';
+export { AdminAccessService } from './admin-access-service.js';
 export { SettingsService } from './settings-service.js';
 export { PermissionsService } from './permissions-service.js';
 export { ModuleService } from './lms-module-service.js';
